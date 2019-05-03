4db08d3450b438bd15485cb4bc9e131f9367450c
Maryam Biazaran <maryam.biazaran@gmail.com>
2019-05-03 09:40:01 -0500

    updated to use templates

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from caesar import rotate_string 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-page_header = """
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title> ::: Web Caesar ::: </title>
-            <style>
-                form {
-                    background-color: #eee;
-                    padding: 20px;
-                    margin: 0 auto;
-                    width: 540px;
-                    font: 16px sans-serif;
-                    border-radius: 10px;
-                }
-                textarea {
-                    margin: 10px 0;
-                    width: 540px;
-                    height: 120px;
-                }
-                p {
-                    color: #909090
-                }
-            </style>
-        </head>
-        <body>
-"""
-page_footer = """
-        </body>
-    </html>
+string1 =''
 
-"""
-form = """
-    <form action='/encrypted' method='post' >
-        <label for='rotation-value'> Rotate by:
-            <input type='text' id='rotation-value' name='rot' value='0' />
-        </label>
-        <br>
-        <textarea name='text'></textarea>
-        <input type='submit' value= 'Go'/>
-    </form>
-"""
 
-page_middle = '<p> Welcome to my app! </p>'
-
-@app.route('/encrypted', methods = ['POST'])
+@app.route('/encrypt', methods = ['POST'])
 def encrypt():
     text_to_rotate = request.form['text']
     rotation = request.form['rot']
@@ -55,11 +14,8 @@ def encrypt():
     return '<h1>{}</h1>'.format(encrypted_text)
     
 
-
-
 @app.route("/")
 def index():
-    content = page_header + page_middle + form + page_footer
-    return content
+    return render_template('form.html',bigbox = string1)
 
 app.run()
